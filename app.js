05ead90fce693a184dc949725872ebf36fe3ce6d
dmp03david <dx02305@gmail.com>
2021-07-12 15:24:48 +0200

    Add /rocket/{id} detail rocket

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.get('/rockets-redis', async (req, res, next) => {
             res.send(JSON.parse(reply))
             return
         }
-        const response = await axios.get('https://api.spacexdata.com/v4/rockets')
+        const response = await axios.get('https://api.spacexdata.com/v3/rockets')
         const saveResult = await SET_ASYNC(
             'rockets',
             JSON.stringify(response.data),
@@ -49,6 +49,27 @@ app.get('/rockets-redis', async (req, res, next) => {
     }
 });
 
+app.get('/rocket/:rocket_id', async (req, res, next) =>{
+    try {
+        const {rocket_id} = req.params
+        const reply = await GET_ASYNC(rocket_id)
+        if (reply) {
+            console.log('Using cached data')
+            res.send(JSON.parse(reply))
+            return
+        }
+        const response = await axios.get(`https://api.spacexdata.com/v3/rockets/${rocket_id}`)
+        const saveResult = await SET_ASYNC(
+            rocket_id,
+            JSON.stringify(response.data),
+            'EX',
+             15)
+        console.log('New data Cached', saveResult)
+        res.send(response.data)
+    }catch (err) {
+        res.send(err.message)
+    }
+})
 
 
 app.listen(3000, () => console.log('🚀 on port 3000'));
